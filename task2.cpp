21d7266d21d49bc5c1cc994074267c48912c15e5
Sergey Lyulchenko <solo300m@yandex.ru>
2023-10-23 10:34:00 +0400

    task_2

--- task2.cpp
+++ task2.cpp
@@ -5,4 +5,35 @@
 using namespace std;
 int main()
 {
+    multimap<string, int> regList;
+    multimap<string, int> &ptrReg = regList;
+    int count = 0;
+    while (true)
+    {
+        string enter;
+        cout << "Введите фамилию и имя через пробел для включения в электронный список или Next для вызова из списка " << endl;
+        cout << "-> ";
+        cin >> enter;
+        if (enter != "Next")
+        {
+            count++;
+            pair<string, int> oP(enter, count);
+            ptrReg.insert(oP);
+        }
+        else
+        {
+            if (count != 0)
+            {
+                multimap<string, int>::iterator it = regList.begin();
+                cout << "<- " << it->first << endl;
+                regList.erase(it);
+                count--;
+            }
+            else
+            {
+                cout << "В списке нет посетителей. Программа завершает работу..." << endl;
+                break;
+            }
+        }
+    }
 }
